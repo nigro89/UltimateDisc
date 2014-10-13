48758855d9261a17be64b3b39814420c5ea7a36c
nigro89 <nigroantonio21@gmail.com>
2014-10-13 15:43:18 +0200

    XX

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,39 @@
+public abstract class Player implements MovingObject {
 
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setDirection() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
 public abstract class Player implements MovingObject {
 
-	// commento commit marco
-	//commento io ora :-)
-	
 	@Override
 	public int getX() {
 		// TODO Auto-generated method stub
